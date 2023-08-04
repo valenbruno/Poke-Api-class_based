6fea4801570b2e08da6328f085d1ce4cc8d6f780
valenbruno1212 <valenbruno1212@gmail.com>
2023-08-04 17:27:20 -0300

    drf

--- PokeApi_Project/api/urls.py
+++ PokeApi_Project/api/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import ListAndCreatePokemon, DeletePokemon, EditPokemon, AdittionalPokemonViews # create_pokemon
+from .views import ListAndCreatePokemon, DeletePokemonAndGetID, EditPokemon, AdittionalPokemonViews # create_pokemon
+
 
 urlpatterns = [
     path('pokemon/', ListAndCreatePokemon.as_view(), name = "create_pokemon"),
-    path('enlace/<str:nombre_enlace>/<str:nombre_campo>/', ListAndCreatePokemon.filtrado, name = 'filtrado'),
-    path('busqueda/',ListAndCreatePokemon.filtrado_busqueda, name = 'filtrado_search'),
-    path('pokemon/<int:pk>/', EditPokemon.as_view() , name = 'edit_pokemon'), # Pide un objeto pk (primary key) si es una vista basada en clase
-    path('pokemon/delete/<int:pk>', DeletePokemon.as_view(), name = 'delete_pokemon'), # pk, las vistas basadas en clases trabajan con objetos
+    path('pokemon/<int:id>/', EditPokemon.as_view() , name = 'edit_pokemon'), # Pide un objeto pk (primary key) si es una vista basada en clase
+    path('pokemon/<int:id>', DeletePokemonAndGetID.as_view(), name = 'delete_pokemon'), # pk, las vistas basadas en clases trabajan con objetos
     path('nemesis/<int:id>', AdittionalPokemonViews.nemesis_pokemon, name = 'nemesis_pokemon'),
     path('remove-tipo/', AdittionalPokemonViews.remove_pokemon, name = 'remove_pokemon'),
     path('enfrentamiento/', AdittionalPokemonViews.enfrentamiento, name = 'enfrentamiento_pokemon'),
